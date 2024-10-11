48f983bd10d688c1cedbc743b766d734540568d7
iCoderPrakash <icoderprakash@gmail.com>
2024-10-11 10:35:32 +0530

    v

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -54,7 +54,9 @@ const Home = () => {
         className="flex flex-col lg:flex-row items-center justify-between bg-gradient-to-br min-h-screen from-gray-700 to-black px-6 py-12"
         ref={heroSection}
       >
+          <div className="">
           <h1 className="text-3xl font-bold text-white text-center block mb-5">Compera Institute</h1>
+          </div>
         <div className="text-white lg:w-1/2 space-y-4">
           <h3 className="text-xl font-medium">Welcome to Compera Institute</h3>
           <h1 className="text-4xl md:text-6xl font-bold">Learn Today,</h1>
